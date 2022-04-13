b73318d9dc896c9b779cde3625c6db87539a2e92
Patrik Martinko <patrik.martinko@gmail.com>
2022-04-13 17:31:21 +0200

    Update

--- website/script.js
+++ website/script.js
@@ -1,3 +1,9 @@
+var options = {
+	type: 'electric',
+	priceElectric: 0.1,
+	consumptionElectric: 0.2,
+	consumption: 7
+};
 var data = null;
 const get = function (url, load) {
 	const request = new XMLHttpRequest();
@@ -6,32 +12,42 @@ const get = function (url, load) {
 	request.send();
 };
 const selectCountry = document.getElementById('country');
-const updateCountry = function () {
+const selectType = document.getElementById('type');
+const update = function () {
+	localStorage.setItem('country', selectCountry.value);
+	localStorage.setItem('type', selectType.value);
 	if (data) {
-		const country = selectCountry.options[selectCountry.selectedIndex].textContent;
-		for (const key in data) {
-			if (data[key][0] == country) {
-				document.getElementById('price').textContent = data[key][1] + '€';
+		if (selectType.value == 'electricity') {
+			document.getElementById('data-current').style = 'display: none';
+		} else {
+			document.getElementById('data-current').style = '';
+			const country = selectCountry.options[selectCountry.selectedIndex].textContent;
+			document.getElementById('selected-type').textContent = '(' + selectType.options[selectType.selectedIndex].textContent + ')';
+			document.getElementById('latest-update').textContent = data[0][0];
+			for (const key in data) {
+				if (data[key][0] == country) {
+					document.getElementById('price').textContent = data[key][selectType.selectedIndex + 1] + '€/l';
+				}
 			}
 		}
 	}
 };
-selectCountry.onchange = updateCountry;
+selectCountry.onchange = update;
+selectType.onchange = update;
 if (!localStorage.getItem('country')) {
 	get('https://ip2c.org/self', function (request) {
 		const country = request.target.responseText.split(';')[1];
 		if (country) {
 			selectCountry.value = country;
-			updateCountry();
-			localStorage.setItem('country', country);
+			update();
 		}
 	});
 } else {
 	selectCountry.value = localStorage.getItem('country');
-	updateCountry();
+	update();
 }
 get('https://script.google.com/macros/s/AKfycby5CcRVznevNoWZeexy0m1DSeOX4Kg1FeMgxFdXlr4sySwgehnIr4T23Q5ooWDtR1iB/exec', function (request) {
 	data = JSON.parse(request.target.responseText);
-	updateCountry();
+	update();
 });
 document.getElementById('copyright').innerHTML = '&copy; ' + (new Date()).getFullYear();
