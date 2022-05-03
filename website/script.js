fab9428f492113525beb30bce4e3d950c111ad3c
Patrik Martinko <patrik.martinko@gmail.com>
2022-05-03 22:07:49 +0200

    Controls

--- website/script.js
+++ website/script.js
@@ -1,52 +1,57 @@
+var data = null;
 var options = {
 	type: 'electric',
 	priceElectric: 0.1,
 	consumptionElectric: 0.2,
 	consumption: 7
 };
-var data = null;
-const get = function (url, load) {
+const get = (url, load) => {
 	const request = new XMLHttpRequest();
 	request.addEventListener('load', load);
 	request.open('GET', url);
 	request.send();
 };
-const selectCountry = document.getElementById('country');
-const selectType = document.getElementById('type');
-const update = function () {
-	localStorage.setItem('country', selectCountry.value);
-	localStorage.setItem('type', selectType.value);
+const controls = document.getElementsByClassName('control');
+const update = () => {
+	for (let control of controls) {
+		localStorage.setItem(control.getAttribute('id'), control.value);
+	}
 	if (data) {
-		if (selectType.value == 'electricity') {
+		if (document.getElementById('type').value == 'electricity') {
 			document.getElementById('data-current').style = 'display: none';
 		} else {
 			document.getElementById('data-current').style = '';
-			const country = selectCountry.options[selectCountry.selectedIndex].textContent;
-			document.getElementById('selected-type').textContent = '(' + selectType.options[selectType.selectedIndex].textContent + ')';
+			const country = document.getElementById('country').options[document.getElementById('country').selectedIndex].textContent;
+			document.getElementById('selected-type').textContent = '(' + document.getElementById('type').options[document.getElementById('type').selectedIndex].textContent + ')';
 			document.getElementById('latest-update').textContent = data[0][0];
-			for (var key in data) {
+			for (let key in data) {
 				if (data[key][0] == country) {
-					document.getElementById('price').textContent = data[key][selectType.selectedIndex + 1] + '€/l';
+					document.getElementById('price').textContent = data[key][document.getElementById('type').selectedIndex + 1] + '€/l';
 				}
 			}
 		}
 	}
 };
-selectCountry.onchange = update;
-selectType.onchange = update;
-if (!localStorage.getItem('country')) {
-	get('https://ip2c.org/self', function (request) {
-		const country = request.target.responseText.split(';')[1];
-		if (country) {
-			selectCountry.value = country;
-			update();
-		}
-	});
-} else {
-	selectCountry.value = localStorage.getItem('country');
-	update();
+for (let control of controls) {
+	if (localStorage.getItem(control.getAttribute('id'))) {
+		control.value = localStorage.getItem(control.getAttribute('id'));
+	}
+	control.onchange = update;
 }
-get('https://script.google.com/macros/s/AKfycby5CcRVznevNoWZeexy0m1DSeOX4Kg1FeMgxFdXlr4sySwgehnIr4T23Q5ooWDtR1iB/exec', function (request) {
+// document.getElementById('country').prepend('<option>Detecting country</option>');
+// if (!localStorage.getItem('country')) {
+// 	get('https://ip2c.org/self', function (request) {
+// 		const country = request.target.responseText.split(';')[1];
+// 		if (country) {
+// 			selectCountry.value = country;
+// 			update();
+// 		}
+// 	});
+// } else {
+// 	selectCountry.value = localStorage.getItem('country');
+// 	update();
+// }
+get('https://script.google.com/macros/s/AKfycby5CcRVznevNoWZeexy0m1DSeOX4Kg1FeMgxFdXlr4sySwgehnIr4T23Q5ooWDtR1iB/exec', request => {
 	data = JSON.parse(request.target.responseText);
 	update();
 });
