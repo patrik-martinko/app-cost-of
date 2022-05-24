276ca5c8abe0c9eb73f27b36baa8c46322bedb4d
Patrik Martinko <patrik.martinko@gmail.com>
2022-05-24 19:38:53 +0200

    Package script

--- package.ps1
+++ package.ps1
@@ -7,7 +7,6 @@ If ($Version -eq 2) {
 	Compress-Archive -Path extension\* -DestinationPath ~\Downloads\extension-v2.zip
 	Rename-Item -Path extension\manifest.json -NewName manifest-v2.json
 	Rename-Item -Path extension\manifest-v3.json -NewName manifest.json
-
 }
 Else {
 	Compress-Archive -Path extension\* -DestinationPath ~\Downloads\extension.zip
