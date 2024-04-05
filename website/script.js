7a81556092ce89d8783511afcb444dacd9d8d727
Patrik Martinko <patrik.martinko@gmail.com>
2024-04-05 17:30:43 +0200

    PWA / Share

--- website/script.js
+++ website/script.js
@@ -268,7 +268,7 @@ onAuthStateChanged(auth, (user) => {
 });
 if (params.get('share')) {
 	show('route');
-	const share = (/(.*km).*(https:\/\/.*)/gm).exec(params.get('share'));
+	const share = (/.*km.*(https:\/\/.*)/gm).exec(params.get('share'));
 	if (share) {
 		get('route-description').textContent = share[1];
 	}
