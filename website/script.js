af0c71c62e47c9114f859e6ed4ce0d5a33426865
Patrik Martinko <patrik.martinko@gmail.com>
2026-04-27 16:11:15 +0200

    Refine user agent detection for button link assignment

--- website/script.js
+++ website/script.js
@@ -187,7 +187,7 @@ document.addEventListener('AddonCostOf', () => {
 	get('button-get').textContent = 'Open Google Maps';
 	get('button-get').setAttribute('href', 'https://maps.google.com');
 });
-if (navigator.userAgentData && navigator.userAgentData.brands.some(b => b.brand === 'Chromium')) {
+if (navigator.userAgentData && !navigator.userAgentData.mobile && navigator.userAgentData.brands.some(b => b.brand === 'Chromium')) {
 	get('button-get').setAttribute('href', 'https://chrome.google.com/webstore/detail/cost-of-driving-in-google/glajpeclpoeodmfofkelgedjphkdgmie');
 } else {
 	get('button-get').setAttribute('href', 'https://addons.mozilla.org/en-US/firefox/addon/cost-of-driving-in-google-maps');
